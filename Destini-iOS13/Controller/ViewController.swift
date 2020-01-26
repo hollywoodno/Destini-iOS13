150f06020fadc5b30bd4dccf4dd820b6dc599973
hollywoodno <hollywoodno@gmail.com>
2020-01-25 18:11:42 -0800

    track current story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,23 +14,42 @@ class ViewController: UIViewController {
   @IBOutlet weak var choice1Button: UIButton!
   @IBOutlet weak var choice2Button: UIButton!
   
-  let story = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right")
+  let stories = [
+    Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right"),
+    Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+    Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+  ]
+  
+  var storyNumber = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    storyLabel.text = story.title
+    let currentStory = stories[storyNumber]
+    storyLabel.text = currentStory.title
+    choice1Button.setTitle(currentStory.choice1, for: .normal)
+    choice2Button.setTitle(currentStory.choice2, for: .normal)
   }
   
   @IBAction func choiceMade(_ sender: UIButton) {
     let selected = sender.currentTitle
     
-    if selected == "Choice 1" {
-      sender.setTitle(story.choice1, for: .normal)
+    if selected == stories[storyNumber].choice1 {
+      
+      storyNumber += 1
+      storyLabel.text = stories[storyNumber].title
+      
+      // update choices to match new story
+      choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+      choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
     } else {
-      sender.setTitle(story.choice2, for: .normal)
+      storyNumber += 2
+      storyLabel.text = stories[storyNumber].title
+      
+      // update choices to match new story
+      choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+      choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
     }
   }
-  
 }
 
